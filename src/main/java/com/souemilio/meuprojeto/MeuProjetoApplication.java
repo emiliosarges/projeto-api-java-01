083cef08077f3c6720426b3688dfc57b5ba28eb4
emiliosarges <emiliosarges@gmail.com>
2026-03-05 23:38:38 -0300

    iniciando o projeto

---
+++ src/main/java/com/souemilio/meuprojeto/MeuProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.souemilio.meuprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souemilio/meuprojeto/MeuProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souemilio.meuprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
